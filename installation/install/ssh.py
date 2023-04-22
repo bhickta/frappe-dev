611d8d86114df3e167d4d8d01a95c195f5b096ad
bhickta <nishantbhickta@gmail.com>
2023-04-22 12:24:48 +0530

    domain and ssh

---
+++ installation/install/ssh.py
@@ -0,0 +1,27 @@
+import os
+import click
+
+
+def domain():
+    commands = [
+        'bench setup add-domain',
+        'bench setup nginx',
+        'sudo service nginx reload',
+        'sudo apt install certbot'
+        ]
+    for command in commands:
+        os.system(command)
+
+@click.command()
+def ssh():
+    commands = [
+        'bench config dns_multitenant on',
+        'bench setup nginx',
+        'sudo service nginx reload'
+    ]
+    for command in commands:
+        os.system(command)
+
+if __name__ == '__main__':
+    domain()
+    ssh()
